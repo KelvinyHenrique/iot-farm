b7f6cbe446a195a06cb528ee8233ac2bf83cb6d3
Kelviny Henrique <kelviny.henrique.ext@infracommerce.com.br>
2022-10-18 18:49:16 -0300

    Basic structure

--- main/iot-farm.c
+++ main/iot-farm.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_log.h"
 
+static const char *TAG = "iot-farm";
 void app_main(void)
 {
-    printf("Hello world!");
 
     while (1)
     {
+        ESP_LOGI(TAG, "Hello world!");
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
